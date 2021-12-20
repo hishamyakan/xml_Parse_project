a36637365b3ec4a976262b2883a9603a850ea5ec
Nada Youssef <85260894+NadaYousseff@users.noreply.github.com>
2021-12-20 23:52:19 +0200

    Source file for error detection and correction functions.

---
+++ correction.cpp
@@ -0,0 +1,398 @@
+#include "correction.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
+#include <vector>
+#include "tag.h"
+#include "Tree.h"
+#include "parse.h"
+#include "Stack.h"
+#include <stack>
+
+
+/*
+ * Description :
+ * sorts a vector of integer that is passed by address
+ * Time Complexity : O(n^2)
+ * Space Complexity : O(1)
+ */
+static void sort(vector<int> &arr){
+
+	int size = arr.size();
+	bool sorted;
+	for(int i = 0 ; i <size ; i++){
+		sorted = true;
+		for(int j = 0 ; j < size-i-1 ; j++){
+
+			if(arr[j] > arr[j+1]){
+				sorted = false;
+				SWAP(arr[j] , arr[j+1]);
+			}
+
+		}
+		if(sorted)
+			return;
+	}
+}
+
+
+
+
+
+/*
+ * Description :
+ * Removes any closing tags at the beginning, opening tags at the end, or data outside the vector bounds.
+ * Time Complexity : O(n)
+ * Space Complexity : O(n)
+ */
+static vector<string> removeBounds(vector<string> v){
+
+	int start = 0 , end = v.size()-1 ;
+
+	//Search for the location of the first Opening Tag.
+	while(!isOpeningTag(v[start]))
+		start++;
+
+	//Search for the location of the last Closing Tag.
+	while(!isClosingTag(v[end]))
+		end--;
+
+	vector<string> res ;
+
+	//Extract this portion from the original vector.
+	for(int i = start ; i <= end ; i++){
+		res.push_back(v[i]);
+	}
+	return res;
+}
+
+
+/*
+ * Description :
+ * Checks if an element is present in a vector.
+ * Returns true if it is present, otherwise it returns false.
+ * Time Complexity : O(n)
+ * Space Complexity : O(1)
+ */
+static bool in(vector<int> arr , int element){
+
+	for(int i = 0 ; i < arr.size() ; i++){
+		if(arr[i] == element)
+			return true;
+	}
+	return false;
+}
+
+
+/*
+ * Description :
+ * Removes incorrect opening and closing tags in the middle of the file.
+ * First, It detects the incorrect tags and their locations.
+ * Then it returns a vector of the tags that are correct.
+ *
+ */
+static vector<string> removeExtraTags(vector<string> v){
+
+	Stack<Tag> s;
+	vector<int> indices;
+	vector<Tag> TAGS;
+	Tag temp ;
+
+	/* For every tag in the vector: Create an object of TAG,
+	 * assign its name and location(index) in the vector*/
+	for(int i = 0  ; i <(int)v.size() ; i++){
+		temp.name = v[i];
+		temp.loc = i ;
+		TAGS.push_back(temp);
+	}
+
+	/*Starting from here, error detection is performed on each tag as follows:
+	 * If its is a comment, prolog or self closing tag, it is ignored.
+	 *  If the tag is an opening tag, it is pushed on the stack.
+	 *  If it is a closing tag,then we check if it matches the top of the stack.
+	 *  If they do match, the opening tag is popped from the stack.
+	 *  If they don't, One of the two tags is misplaced.
+	 *  Detecting which one is the incorrect tag starts at line ...
+	 *  When an incorrect tag is detected, its index is pushed in a vector of indices.
+	 *
+	 */
+
+	for(int i = 0 ; i <(int)v.size() ; i++){
+
+		string tag = v[i];
+
+		if(isComment(tag) || isProlog(tag) || isSelfClosingTag(tag))
+			continue;
+
+		else if(isOpeningTag(tag)){
+
+			s.push(TAGS[i]);
+
+		}
+
+		else if(isClosingTag(tag)){
+			//cout<<"The top of the stack now is "<<s.top().name<<endl;
+			//cout<<"The closing tag now is "<<tag<<endl;
+			if(!areMatching(s.top().name , tag )){
+
+				cout<<"Error detected"<<endl;
+				cout<<"Opening Tag: "<<s.top().name<<endl;
+				cout<<"Closing Tag: "<<tag<<endl;
+
+				/*When a mismatch occurs, we create a copy of the stack
+				 * and check whether there exists an opening tag that
+				 * matches the current closing tag.*/
+
+				Stack<Tag> temp = s;
+
+				int count = 0 ;
+
+
+				while(!temp.isEmpty()){
+
+					if(areMatching(temp.top().name , tag )){
+						break;
+					}
+					temp.pop();
+					count++;
+
+				}
+
+				//If there's no matching opening tag, that means that the closing tag is wrong.
+				if(temp.isEmpty()){
+					//Mafish opening tag lay2a 3aleha
+					//Di makanha ghalat ka closing tag
+					indices.push_back(i);
+				}
+
+				//Otherwise, the previously popped opening tags are wrong.
+				else{
+					while(count){
+						Tag t = s.pop();
+
+						//cout<<"Error in "<<t.name<<" and the count is "<<count<<endl;
+						indices.push_back(t.loc);
+						count-- ;
+						cout<<"The top of the stack after error is "<<s.top().name<<endl;
+						s.pop();
+					}
+					//Kol el opening tags ely etshaalet di kanet ghalat
+				}
+
+
+			}
+			else
+				s.pop();
+		}
+
+	}
+
+
+	cout<<"Error List: "<<endl;
+	for(int i = 0 ; i < indices.size() ; i++){
+		cout<<v[indices[i]]<<endl;
+	}
+	cout<<"End of error list"<<endl;
+
+
+	vector<string> finalists;
+
+	sort(indices);
+	int i = 0 ;
+
+	/*The last step is to select the correct tags only from the original vector.*/
+	for(int i = 0 ; i < (int)v.size() ; i++){
+
+		/*If the current index is in the list of error indices, ignore this tag*/
+		if(in(indices , i) )
+			continue;
+		finalists.push_back(v[i]);
+
+	}
+
+	return finalists;
+
+}
+
+
+/*
+ * Description :
+ * Converts a vector of correct tags and data to a tree and returns a pointer to its root.
+ * Time Complexity : O(n)
+ * Space Complexity : O(n)
+ */
+static Node* correctTree(vector<string> v){
+	/*A stack for pushing nodes*/
+	Stack<Node*> myStack;
+
+	Node *root = nullptr;
+
+	/*For every element in the vector: */
+	for(int i = 0 ; i <(int)v.size() ; i++){
+
+		if(isProlog(v[i]) || isComment(v[i])||isSelfClosingTag(v[i]))
+			continue;
+		/*If it is an opening tag: */
+		if(isOpeningTag(v[i])){
+
+			/*Create a new node, assign its tag name with the current tag,
+			  and push it on the stack.*/
+			Node *ptr = new Node();
+
+			ptr->setTagName(v[i]);
+
+			myStack.push(ptr);
+		}
+
+		/*If it is a closing tag: */
+
+		else if(isClosingTag(v[i])){
+
+			/*Pop the last node from the stack*/
+			Node *temp = myStack.pop();
+
+			/*If it is the last node, then it is the root of the tree.*/
+			if(myStack.isEmpty()){
+				root = temp;
+			}
+
+			/*Otherwise, then it is the child of node below it.*/
+			else{
+				myStack.top()->addChild(temp);
+			}
+
+		}
+
+		/*If it is not a tag, then assign this data to the node at the top of the stack*/
+		else{
+			myStack.top()->setData(v[i]);
+
+		}
+
+
+	}
+
+	return root;
+}
+
+
+/*
+ * Description :
+ * Converts a file to a tree after correction and returns a pointer to its root.
+ * Time Complexity : O(n^2)
+ * Space Complexity : O(n)
+ */
+Node* correctTree(string file_path){
+
+	return correctTree(removeExtraTags(removeBounds(FileToVector(file_path))));
+
+}
+
+
+/*
+ *Description :
+ *function that takes the file path to detect the errors returning error line and the wrong tag
+ */
+vector<Tag2> Errors(string file_path) {
+	vector<Tag2> errors; //to be returned
+	vector<string> tag_str;
+	stack<Tag2> st;
+	stack<Tag2> copyst;
+	bool matchingWithCopy = false;
+	int sizeCopy = 0;
+	int line_loc = 0;
+	string line;
+	ifstream t;
+	Tag2 temp;
+	vector<Tag2> vect;
+	vector<Tag2> Copy;
+	t.open(file_path, ios::in);
+
+	if (t.is_open()) {
+
+		while (getline(t, line)) {
+			line_loc++;
+			tag_str = getTagsFromLine(line);
+			for (int i = 0; i < tag_str.size(); i++) {   //copy the tags and the line location
+				temp.tag = tag_str[i];
+				temp.line = line_loc;
+				vect.push_back(temp);
+			}
+		}
+		t.close();
+
+	}
+	for (int i = 0; i < vect.size(); i++) {
+		if (isProlog(vect[i].tag) || isComment(vect[i].tag))
+			continue;
+		if (isOpeningTag(vect[i].tag)) {
+			st.push(vect[i]);
+		}
+		if (isClosingTag(vect[i].tag)) {
+			if (!st.empty()) {
+				bool matching = areMatching(st.top().tag, vect[i].tag);
+				if (matching) {
+					st.pop();
+
+				}
+				else {                                   //mismatching
+					int k;
+					copyst = st; //making a copy of the stack
+					sizeCopy = copyst.size();
+					for (k = 0; k < sizeCopy; k++) { //iteration on the new stack
+						matchingWithCopy = areMatching(copyst.top().tag, vect[i].tag);
+						if (matchingWithCopy)
+						{
+							copyst.pop();
+							break;
+						}
+
+						copyst.pop();
+					}
+					if (!matchingWithCopy)
+					{
+
+
+						temp = vect[i];
+						errors.push_back(temp);
+					}
+					if (matchingWithCopy)
+					{
+						for (int i = 0; i < k; i++) {
+							Copy.push_back(st.top());
+							st.pop();
+						}
+						st.pop();
+						for (int k = 0; k < Copy.size(); k++) {
+
+							temp = Copy[k];
+
+							errors.push_back(temp);
+
+						}
+
+					}
+				}
+
+
+			}
+			Copy.clear();
+		}
+
+	}
+
+	if (!st.empty()) {
+		int d = st.size();
+		for (int n = 1; n <= d; n++) {
+
+			temp = st.top();
+			errors.push_back(temp);
+			st.pop();
+		}
+	}
+	return errors;
+}
+
+
+
+
